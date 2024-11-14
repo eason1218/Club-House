50896a0278b6838c4b34886790ba1a2ab9589521
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-11-14 16:51:44 -0500

    Update 01-test_data.R

--- scripts/01-test_data.R
+++ scripts/01-test_data.R
@@ -1,14 +1,10 @@
 #### Preamble ####
-# Purpose: Tests the structure and validity of the simulated Australian 
-#electoral divisions dataset.
-# Author: Rohan Alexander
-# Date: 26 September 2024
-# Contact: rohan.alexander@utoronto.ca
+# Purpose: Tests the structure and validity of the simulated data
+# Author: YiZhuo Li
+# Date: 14 November 2024
+# Contact: liyizhuo.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - The `tidyverse` package must be installed and loaded
-# - 00-simulate_data.R must have been run
-# Any other information needed? Make sure you are in the `starter_folder` rproj
+
 #### Workspace setup ####
 library(tidyverse)
 
