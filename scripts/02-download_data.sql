ea981f9085751f8fa09e5469e4e1557b88fa50d0
YiZhuo Li <52457749+eason1218@users.noreply.github.com>
2024-11-14 15:33:48 -0500

    Update 02-download_data.sql

--- scripts/02-download_data.sql
+++ scripts/02-download_data.sql
@@ -1,3 +1,12 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from [...UPDATE THIS...]
+# Author: Rohan Alexander [...UPDATE THIS...]
+# Date: 11 February 2023 [...UPDATE THIS...]
+# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# License: MIT
+# Pre-requisites: [...UPDATE THIS...]
+# Any other information needed? [...UPDATE THIS...]
+
 SELECT r.*, p.product_name
 FROM product p
 JOIN raw r ON p.id = r.product_id
